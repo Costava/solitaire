e9ef2bd069c72ab601998b64ccfa588c91cd65db
Costava <Costava@users.noreply.github.com>
2016-12-29 20:29:47 -0600

    Change encode script

--- encode.js
+++ encode.js
@@ -1,15 +1,23 @@
 /**
- * Outputs the data uri of the image
+ * Outputs the data uri of the file
  * Usage: `node encode.js path/to/image.png`
  */
 
 const DataURI = require('datauri');
 const datauri = new DataURI();
 
-datauri.encode(process.argv[2], (err, content) => {
-	if (err) {
-			throw err;
-	}
+var target = process.argv[2];
 
-	console.log(content);
-});
+if (target == undefined) {
+	console.log("No file specified")
+}
+else {
+	datauri.encode(target, (err, content) => {
+		if (err) {
+			throw err;
+		}
+
+		console.log(content);
+	});
+
+}
